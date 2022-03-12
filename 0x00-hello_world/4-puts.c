d2e48cba3605de4bf7ccfbd16659f8dd4752734d
root <yemitemi17@gmail.com>
2022-03-12 01:31:58 -0800

    my shell

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is lik
+e building a multilingual puzzle"
+);
+	return(0);
+}
