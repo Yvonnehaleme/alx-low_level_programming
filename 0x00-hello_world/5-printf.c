9636ea41fa5238cf84bde22e6f5e9a63bb92c2ed
root <yemitemi17@gmail.com>
2022-03-13 11:13:41 -0700

    my shell

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/** 
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (success)
@@ -7,7 +8,7 @@
 int main(void)
 {
        printf("with proper grammar, b
-ut the outcome is a piece of art,\n"
+		       ut the outcome is a piece of art,\n"
 	     );
 		       return (0);
 }
