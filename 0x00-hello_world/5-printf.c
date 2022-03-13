40c01c739903c60910a81d99c4efe9b578f1af24
Opeyemi Odusanya <95849144+Yvonnehaleme@users.noreply.github.com>
2022-03-13 19:50:32 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-       printf("with proper grammar, b
-		       ut the outcome is a piece of art,\n"
-	     );
-		       return (0);
-}
+ 
+ ​#​include​ ​<​stdio.h​> 
+ ​/*​* 
+ ​ * main - main block 
+ ​ * Return: 0 
+ ​ ​*/ 
+ ​int​ ​main​(​void​) 
+ ​{ 
+ ​printf​(​"​with proper grammar, but the outcome is a piece of art,​\n​"​); 
+ ​return​ (​0​); 
+ ​}
