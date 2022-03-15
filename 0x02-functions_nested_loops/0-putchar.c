a78a53cde449cdee2dc9e9a42b32757226489a3a
opeyemi <yemitemi17@gmail.com>
2022-03-15 08:44:41 -0700

    my shell

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return:0
- *
+ * main - prints Holberton, folowed by newline.
+ * Return: 0 (success)
  */
 int main(void)
 {
-char c[10] = "Holberton";
-int i = 0;
-while (i < 9)
-{
-putchar(c[i]);
-i++;
-}
-putchar('\n');
-return (0);
+	char *holberton;
+
+	for (holberton = "Holberton\n"
+;*holberton != '\0'; ++holberton);
+	       putchar(holberton);
+	return (0);
 }
