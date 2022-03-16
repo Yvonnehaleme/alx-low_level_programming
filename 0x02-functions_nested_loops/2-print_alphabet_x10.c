29e321875bbc017ba08f3182e10c1d8c5861966b
opeyemi <yemitemi17@gmail.com>
2022-03-16 02:23:13 -0700

    my shell

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+
+#include "main.h"
+/**  
+ * print_alphabet_x10 - prints 10 times the alphabet,
+ * in lowercase, followed by a new line. 
+ * 
+ * Return: void function
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int coubter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		(alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+
+}
